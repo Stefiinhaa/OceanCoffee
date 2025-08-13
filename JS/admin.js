38941aeefebb8d9cf956fcd4d4da42212603d543
Stefani <152235460+Stefiinhaa@users.noreply.github.com>
2025-08-13 14:57:03 -0300

    Delete JS directory

--- JS/admin.js
+++
@@ -1,8 +0,0 @@
-const menuItems = document.querySelectorAll('.menu-item');
-
-    menuItems.forEach(item => {
-      item.addEventListener('click', () => {
-        menuItems.forEach(i => i.classList.remove('selected'));
-        item.classList.add('selected');
-      });
-    });
