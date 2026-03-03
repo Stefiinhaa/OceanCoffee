0271a5d58e2e200f247ab028cdfd841ac4bf4caf
Stefani <stefanisantos1212@gmail.com>
2026-03-03 16:30:47 -0300

    d

--- OceanCooffe Stefani_27.08/sw.js
+++ OceanCooffe Stefani_27.08/sw.js
@@ -1,40 +1,31 @@
 // sw.js - Motor de Notificações da Ocean Coffee
 
 self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Força a instalação imediata
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim()); // Assume o controle da página na hora
+    event.waitUntil(clients.claim());
 });
 
-// ESCUTAR O PUSH DO SERVIDOR
 self.addEventListener('push', function(event) {
     let msg = "Nova atualização da Ocean Coffee!";
     
     if (event.data) {
-        msg = event.data.text(); // Pega o texto enviado pelo servidor
+        msg = event.data.text();
     }
 
-    const options = {
-        body: msg,
-        icon: 'IMG/Logo.png',
-        badge: 'IMG/Logo.png',
-        vibrate: [100, 50, 100],
-        data: {
-            url: '/' // Abre a home ao clicar
-        }
-    };
-
     event.waitUntil(
-        self.registration.showNotification('Ocean Coffee', options)
+        self.registration.showNotification('Ocean Coffee', {
+            body: msg,
+            icon: 'IMG/Loginho2.png',
+            badge: 'IMG/Loginho2.png',
+            vibrate: [200, 100, 200, 100, 200]
+        })
     );
 });
 
-// Ação ao clicar na notificação
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data.url)
-    );
+    event.waitUntil(clients.openWindow('/'));
 });
